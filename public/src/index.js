86d321b388a8dd4445d60102e1d689687d7d3d44
sharonchen9593 <sharonchen9593@gmail.com>
2017-06-20 00:15:50 -0700

    connected react

---
+++ public/src/index.js
@@ -0,0 +1,13 @@
+class App extends React.Component {
+
+	
+	render() {
+		return (
+			<div>
+				Hello from React
+			</div>
+	);
+	}
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'))
